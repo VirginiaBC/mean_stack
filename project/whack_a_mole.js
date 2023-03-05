4ac38ab754773639ce765a2980d34ef416733861
Virginia Wu <virginiawu@Virginias-Air.attlocal.net>
2023-03-04 18:15:20 -0800

    add advanced function of mine

--- project/whack_a_mole.js
+++ project/whack_a_mole.js
@@ -14,9 +14,12 @@ const View = (() => {
         let molesDoms = ""
         for (var i=0; i<statusArr.length; i++) {
             const is_show = statusArr[i]
-            if (is_show) {
+            if (is_show == 1) { // Is a mole
                 molesDoms += `<div class="circle" id="hole_${i}" style="background-image: url('mole.png'); background-position: center;""></div>`
-            } else {
+            } else if (is_show == 2) { // Is a snake
+                molesDoms += `<div class="circle" id="hole_${i}" style="background-image: url('mine.png'); background-position: center;""></div>`
+            }
+            else {
                 molesDoms += `<div class="circle" id="hole_${i}"></div>`
             }
         }
@@ -41,7 +44,10 @@ const Model = ((view) => {
 
     class State {
         constructor(listener, timeLeft) {
-            this._moleStatus = new Array(12).fill(false)
+            // After adding snakes, the moleStatus and moleHolePopUpTime
+            // also indicate the status and popup time of snake
+            this._moleStatus = new Array(12).fill(0)
+            this._moleHolePopUpTime = new Array(12).fill(timeLeft)
             this._listener = listener
             this._totalTime = timeLeft
             this._timeLeft = timeLeft
@@ -50,7 +56,8 @@ const Model = ((view) => {
         }
 
         clear() {
-            this.moleStatus = new Array(12).fill(false)
+            this.moleStatus = new Array(12).fill(0)
+            this.moleHolePopUpTime = new Array(12).fill(this._totalTime)
             this.timeLeft = this._totalTime
             this.score = 0
         }
@@ -62,6 +69,14 @@ const Model = ((view) => {
         set moleStatus(status) {
             this._moleStatus = status
             this.renderMoles()
+        }
+
+        get moleHolePopUpTime() {
+            return this._moleHolePopUpTime
+        }
+
+        set moleHolePopUpTime(popUpTime) {
+            this._moleHolePopUpTime = popUpTime
         }
 
         get timeLeft() {
@@ -123,6 +138,11 @@ const Controller = ((view, model) => {
     const {State} = model
     const {domSelector} = view
 
+    let popupMole; // Expose popupMole interval function
+    let popupSnake; // Expose popupSnake interval function
+    let countDown; // Expose countDown interval function
+
+
     const holeListener = (event) => {
         if (state.timeLeft == 0) {
             return
@@ -130,15 +150,21 @@ const Controller = ((view, model) => {
         event.target.id;
         const status = state.moleStatus;
         const index = event.target.id.replace('hole_', '');
-        if (status[index]) {
-            status[index] = false
+        if (status[index] == 1) {
+            status[index] = 0
             state.winAScore()
+        }
+        else if (status[index] == 2) {
+            clearInterval(popupMole)
+            clearInterval(popupSnake)
+            clearInterval(countDown)
+            state.moleStatus = new Array(12).fill(2)
+            return
         }
         state.moleStatus = status
     }
 
     const state = new State(holeListener, 30)
-
 
     const popUpMole = () => {
         if (state.timeLeft == 0) {
@@ -146,25 +172,56 @@ const Controller = ((view, model) => {
         }
         const status = state.moleStatus;
         const emptyIndexes = status
-            .map((elem, index) => elem === false ? index : -1)
+            .map((elem, index) => elem === 0 ? index : -1)
             .reduce((acc, curr) => curr !== -1 ? acc.concat(curr) : acc, []);
-        if (emptyIndexes.length <= 9) {
+        const nonEmptyIndexes = status
+            .map((elem, index) => elem === 0 ? -1 : index)
+            .reduce((acc, curr) => curr !== -1 ? acc.concat(curr) : acc, []);
+        const popUpTime = state.moleHolePopUpTime;
+        // Find out moles and remove it if it's time-out
+        for (let index of nonEmptyIndexes) {
+            if (status[index] == 1 && popUpTime[index] - state.timeLeft >= 3) {
+                status[index] = 0
+            }
+        }
+        // Select a random empty hole to popup a mole
+        const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
+        status[randomIndex] = 1
+        popUpTime[randomIndex] = state.timeLeft
+        state.moleStatus = status
+    }
+
+    const popUpSnake = () => {
+        if (state.timeLeft == 0) {
             return
         }
-        const randomIndex = emptyIndexes[Math.floor(Math.random() * emptyIndexes.length)];
-        status[randomIndex] = true
+        const status = state.moleStatus;
+        const nonSnakeIndexes = status
+            .map((elem, index) => elem === 2 ? -1 : index)
+            .reduce((acc, curr) => curr !== -1 ? acc.concat(curr) : acc, []);
+        // Remove previous snake
+        for (let index in status) {
+            if (status[index] == 2) {
+                status[index] = 0
+            }
+        }
+        // Pop up a new snake
+        const randomIndex = nonSnakeIndexes[Math.floor(Math.random() * nonSnakeIndexes.length)];
+        status[randomIndex] = 2
         state.moleStatus = status
     }
 
     domSelector.startBtn.addEventListener('click', () => {
         state.clear()
         domSelector.startBtn.hidden = true
-        const popup = setInterval(popUpMole, 1000)
-        const countDown = setInterval(() => {
+        popupMole = setInterval(popUpMole, 1000)
+        popupSnake = setInterval(popUpSnake, 2000)
+        countDown = setInterval(() => {
             if (state.timeLeft == 0) {
                 state.renderScoreDom()
                 clearInterval(countDown)
-                clearInterval(popup)
+                clearInterval(popupMole)
+                clearInterval(popupSnake)
                 alert("Time Out! Your score is " + state.score)
                 domSelector.startBtn.hidden = false
                 return 
@@ -172,7 +229,6 @@ const Controller = ((view, model) => {
             state.countDown()
         }, 1000);
     });
-
 
     const bootstrap = () => {
         console.log(domSelector.moleHoles)
